6fd7f523b1e314f6dd1197aaf877e5892e44fb53
tech-naushad <Naushad79@yahoo.com>
2025-06-29 01:17:18 +0400

    added code shared apckage appaorch

--- services/property-service/src/routes/propertyRoutes.js
+++ services/property-service/src/routes/propertyRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../../../../shared/middlewares/contentTypeHandler');
+//const upload = require('../../../../shared/middlewares/contentTypeHandler');
+const upload = require('@mini-zillow/shared/middlewares/contentTypeHandler');
 const {create, getAll, getById, update, remove} = require('../controllers/propertyController');
 
 router.post('/', upload.single('file'), create);
