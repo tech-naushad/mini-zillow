f5c9b868efe95e4fe8fe43c896f118cd0505352a
tech-naushad <Naushad79@yahoo.com>
2025-06-29 01:36:33 +0400

    move the mongodb connection to server file

--- services/property-service/app.js
+++ services/property-service/app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { connectDB } = require("./src/config/db");
+//const { connectDB } = require("./src/config/db");
 const propertyRoutes = require("./src/routes/propertyRoutes");
 
 const app = express();
-connectDB();
+//connectDB();
 
 app.use(cors());
 app.use(express.json());
--- services/property-service/server.js
+++ services/property-service/server.js
@@ -1,6 +1,23 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5001;
+const { connectDB } = require("./src/config/db");
 
-app.listen(PORT, () => {
-  console.log(`Property service running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Property service running on port ${PORT}`);
+// });
+
+const start = async () => {
+  try {
+    await connectDB();  // wait for connection before continuing
+    console.log("✅ MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+};
+
+start();
